dd83a7765c94d253244042f382622af929dfe03c
yicheng <165935874+Vision-mark@users.noreply.github.com>
2025-02-03 00:12:11 +0800

    新增檔案

---
+++ update_excel.bat
@@ -0,0 +1,18 @@
+::要先初始化
+::git init
+::git remote add origin https://github.com/你的GitHub帳號/你的儲存庫.git
+
+@echo off
+cd /d "C:\Users\yiche\Desktop\web_train"
+
+git config --global credential.helper store
+
+git remote add origin https://github.com/cyc-design/movie_design_1.git 2>nul
+
+git add movie_images
+
+git add movie.xlsx
+git commit -m "自動上傳 Excel 檔案 及 圖片資料夾"
+git push origin master
+
+pause
